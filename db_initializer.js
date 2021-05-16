378947d7d0e3d57411bd4bbb3ecd1dc52159aea1
Nipurna Dhakal <nidhakal@clarku.edu>
2021-05-16 16:02:01 -0400

    Made the project framework ->Frontend and Backend. Created the homepage (Frontend)

--- db_initializer.js
+++ db_initializer.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const rawdata = fs.readFileSync(__dirname + "/data.json");
 jsonList = JSON.parse(rawdata);
 
+const csmajordata = fs.readFileSync(__dirname + "/dataCSmajor.json");
+jsonList1 = JSON.parse(csmajordata);
+
+
 mongoose.connect('mongodb://localhost:27017/courseDB', {useNewUrlParser: true}, function () {
     console.log("db connected successful!")
 });
@@ -24,9 +28,13 @@ const courseSchema = new mongoose.Schema({
     Room: String,
 })
 
+
 const Course = mongoose.model('Course', courseSchema);
+const CScourse = mongoose.model('CScourse', courseSchema);
+
 
 courseList = []
+CScourseList = []
 
 jsonList.forEach(function (course) {
     courseList.push({
@@ -46,7 +54,34 @@ jsonList.forEach(function (course) {
     });
 });
 
+jsonList1.forEach(function (course) {
+    CScourseList.push({
+        "CRN": course["CRN"],
+        "Course_num": course["Course_num"],
+        "Sc": course["Sc"],
+        "Title": course["Title"],
+        "Attribute": course["Attribute"],
+        "Units": course["Units"],
+        "CAP": course["CAP"],
+        "Enr": course["Enr"],
+        "Instructor": course["Instructor"],
+        "Modality": course["Modality"],
+        "Days": course["Days"],
+        "Times": course["Times"],
+        "Room": course["Room"],
+    });
+});
+
 Course.insertMany(courseList, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("All data saved");
+        mongoose.connection.close();
+    }
+});
+
+CScourse.insertMany(courseList, function (err) {
     if (err) {
         console.log(err);
     } else {
