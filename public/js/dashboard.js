a387118ea34b5e921855293c7d404d2f4af77880
Sujhan Ghimire <sujhanghimire@hotmail.com>
2021-05-23 18:00:28 -0400

    added dashboard page

---
+++ public/js/dashboard.js
@@ -0,0 +1,7 @@
+$('.dashHeading')
+    .append(`<div class = "dashName"><h2></h2></div>`)
+    .append(`<div class = 'card dashCard'></div>`)
+
+$('.dashName h2').append(`Welcome, Sujhan Ghimire!`)
+$('.dashCard').append(`Major 1: Computer Science <br> Minor 1: Economics <br> Minor 2: Entrepreneurship and Innovation <br>`)
+
