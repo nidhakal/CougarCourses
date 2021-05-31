434bc73643537573bd0d57444d46ee0018186d5f
Sujhan Ghimire <sujhanghimire@hotmail.com>
2021-05-31 17:33:24 -0400

    remove login,reg and add name when logged in

--- public/js/profile.js
+++ public/js/profile.js
@@ -31,3 +31,15 @@ $(document).ready(function (){
         }})
 })
 
+$(document).ready(function (){
+    $.getJSON('/get_current_user').done(function (data) {
+        console.log(data)
+        if(data['message'] === "success"){
+            $('.login').remove();
+            $('#showname').text(data.data.fullname);
+        }else{
+            $('.logout').remove()
+        }
+    })
+})
+
