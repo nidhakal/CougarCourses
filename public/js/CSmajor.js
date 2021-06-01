944edbffec71b348b874e18a13b265186f7aa600
Nipurna Dhakal <nidhakal@clarku.edu>
2021-05-31 23:55:08 -0400

    Edit Profile

--- public/js/CSmajor.js
+++ public/js/CSmajor.js
@@ -95,5 +95,16 @@ function notTaken(){
     }
 }
 
+$(document).ready(function (){
+    $.getJSON('/get_current_user').done(function (data) {
+        console.log(data)
+        if(data['message'] === "success"){
+            $('.login').remove();
+            $('#showname').text(data.data.fullname);
+        }else{
+            $('.logout').remove()
+        }
+    })
+})
 
 
--- public/js/pls.js
+++ public/js/pls.js
@@ -99,3 +99,15 @@ function PLSnotTaken(){
         }
     }
 }
+
+$(document).ready(function (){
+    $.getJSON('/get_current_user').done(function (data) {
+        console.log(data)
+        if(data['message'] === "success"){
+            $('.login').remove();
+            $('#showname').text(data.data.fullname);
+        }else{
+            $('.logout').remove()
+        }
+    })
+})
